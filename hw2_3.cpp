1c4bb43f50c68ad3ea5a36a3893601035fb314cd
EmptyDust <1422492074@qq.com>
2025-03-11 10:53:13 +0800

    Initial commit

---
+++ hw2_3.cpp
@@ -0,0 +1,195 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define getpch(type) (type*)malloc(sizeof(type))
+#define NOTHING 0
+
+typedef struct pcb{//è¿ç¨ç®¡çå
+	char name[10];//è¿ç¨åå­
+	char state;		//è¿ç¨ç¶æ
+	int ntime;		//è¿ç¨éè¦è¿è¡çæ¶é´
+	int rtime;		//è¿ç¨å·²ç»è¿è¡çæ¶é´
+	struct pcb *link;
+}PCB;
+
+PCB	*ready = NOTHING, *pfend = NOTHING,*p =NOTHING,*stop = NOTHING,*stopend = NOTHING;		//å°±ç»ªéåï¼è¿ç¨æå¥ä½ç½®çåé
+
+int geti()	//ä½¿ç¨æ·ä»è½è¾å¥æ´æ°
+{
+	char ch;
+	int i = 0;
+	fflush(stdin);
+	ch = getchar();
+
+	while(ch == '\n'){
+		//printf("\tfè¾å¥ä¸è½ä¸ºç©º..è¯·éæ°è¾å¥\n");
+		fflush(stdin);
+		ch = getchar();
+	}
+
+	while(ch != '\n'){
+		if(ch > '9' || ch < '0'){
+			printf("\tè¾å¥æè¯¯!!è¾å¥åªè½ä¸ºæ­£æ´æ°ï¼è¯·éæ°è¾å¥...\n");
+			fflush(stdin);
+			i = 0;
+			ch = getchar();
+		}else{
+			i =  i*10 + (ch - '0');
+			ch = getchar();
+		}
+	}
+	return i;
+}
+
+void fcfs()//æå¥è¿ç¨
+{
+	if(!ready ){
+		ready = p;
+		pfend = p;
+	}
+	else{
+		p->link = pfend->link;
+		pfend->link = p;
+		pfend = p;
+	}
+}
+
+void input()/*å»ºç«è¿ç¨æ§å¶åå½æ°*/
+{
+	int i,num;
+	printf("\nè¯·è¾å¥è¿ç¨çä¸ªæ°?");
+	num = geti();
+	for(i=0; i < num; i++)
+	{
+		printf("\nè¿ç¨å·No.%d:\n",i+1);
+		p=getpch(PCB);
+		printf("\nè¾å¥è¿ç¨å:");
+		scanf("%s",p->name);
+		printf("\nè¾å¥è¿ç¨è¿è¡æ¶é´:");
+		p ->ntime = geti();
+		printf("\n");
+		p->rtime=0;
+		p->state='w';
+		p->link=NOTHING;
+		fcfs();
+	}
+}
+
+void disp(PCB *pr)/*å»ºç«è¿ç¨ç°å®å½æ°ï¼ç¨äºæ¾ç¤ºå½åè¿ç¨*/
+{
+	printf("\nname\t state\t ntime\t rtime\t \n");
+	printf("|%s\t",pr->name);
+	printf(" |%c\t",pr->state);
+	printf(" |%d\t",pr->ntime);
+	printf(" |%d\t",pr->rtime);
+	printf("\n");
+}
+
+void check()/*å»ºç«è¿ç¨æ¥çå½æ°*/
+{
+	PCB *pr;
+	if(ready != NOTHING){
+        printf("\n ****å½åæ­£å¨è¿è¡çè¿ç¨æ¯:%s",ready->name);/*æ¾ç¤ºå½åè¿è¡çè¿ç¨*/
+        disp(ready);
+        pr= ready ->link;
+        printf("\n****å½åå°±ç»ªéåç¶æä¸º:\n");/*æ¾ç¤ºå°±ç»ªéåç¶æ*/
+        while(pr!=NOTHING)
+        {
+            disp(pr);
+            pr=pr->link;
+        }
+	}
+	printf("\n****å½åå°±é»å¡éåç¶æä¸º:\n");
+	pr = stop;
+	while(pr!=NOTHING){
+        disp(pr);
+        pr = pr->link;
+	}
+}
+
+void destroy()/*å»ºç«è¿ç¨æ¤éå½æ°(è¿ç¨è¿è¡ç»æï¼æ¤éè¿ç¨)*/
+{
+	printf("\nè¿ç¨[%s]å·²å®æ.\n",ready->name);
+	p = ready;
+	ready = ready->link;
+	free(p);
+}
+
+void running()/*å»ºç«è¿ç¨å°±ç»ªå½æ°(è¿ç¨è¿è¡æ¶é´å°ï¼ç½®å°±ç»ªç¶æ)*/
+{
+	(ready -> rtime)++;
+    check();
+	if(ready->rtime == ready->ntime){
+		destroy();
+		return;
+	}
+}
+
+void block()
+{
+    if(!stop){
+        ready->state = 'b';
+        //å¾è¡¥å¨
+		stop = stopend = ready;
+    }
+    else{
+        ready->state = 'b';
+        //å¾è¡¥å¨
+		stopend = stopend->link = ready;
+    }
+	ready = ready->link;
+	stopend->link = NOTHING;
+}
+
+void wake()
+{
+    if(stop == NOTHING){
+        printf("\né»å¡éåä¸ºç©ºï¼è¯·è¿è¡å¶ä»æä½");
+        return;
+    }
+    else{
+        if(ready != NOTHING){
+            //å¾è¡¥å¨
+			pfend = pfend->link = stop;
+        }
+        else{
+            //å¾è¡¥å¨
+			ready = pfend = stop;
+        }
+        //å¾è¡¥å¨
+		stop = stop->link;
+		pfend->link = NOTHING;
+    }
+}
+
+int main()
+{
+	char ch;
+	input();
+	while(ready != NOTHING || stop != NOTHING)
+	{
+	    if(ready !=NOTHING){
+            printf("\nThe execute name:%s\n",ready ->name);
+            ready ->state = 'R';
+            //check();
+            running();
+            //check();
+	    }
+	    else{
+            check();
+	    }
+		printf("\næié®æ·»å æ°è¿ç¨...sé®é»å¡è¿ç¨...wé®å¤éè¿ç¨\næå¶ä»ä»»æé®ç»§ç»­è¿è¡...");
+		fflush(stdin);
+		ch = getchar();
+		if (ch == 'i' || ch == 'I')
+			input();
+        if (ch == 's' || ch == 'S')
+            block();
+        if (ch == 'w' || ch == 'W')
+            wake();
+	}
+	printf("\n\n è¿ç¨å·²ç»å®æ\n");
+	getchar();
+	return 0;
+}
+
---
+++ hw2_4_rr.cpp
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define getpch(type) (type *)malloc(sizeof(type))
+#define NOTHING 0
+#define TIME 2 // æ¶é´çé¿åº¦
+
+typedef struct pcb
+{                  // è¿ç¨ç®¡çå
+    char name[10]; // è¿ç¨åå­
+    char state;    // è¿ç¨ç¶æ
+    int ntime;     // è¿ç¨éè¦è¿è¡çæ¶é´
+    int rtime;     // è¿ç¨å·²ç»è¿è¡çæ¶é´
+    int etime;     // è¿ç¨å¨æ¬éåå¯è¿è¡çæ¶é´ç
+    struct pcb *link;
+} PCB;
+
+PCB *ready = NOTHING, *pfend = NOTHING, *p = NOTHING; // å°±ç»ªéåï¼è¿ç¨æå¥ä½ç½®çåé
+
+int geti() // ä½¿ç¨æ·ä»è½è¾å¥æ´æ°
+{
+    char ch;
+    int i = 0;
+    fflush(stdin);
+    ch = getchar();
+
+    while (ch == '\n')
+    {
+        fflush(stdin);
+        ch = getchar();
+    }
+
+    while (ch != '\n')
+    {
+        if (ch > '9' || ch < '0')
+        {
+            // printf("\tè¾å¥æè¯¯!!è¾å¥åªè½ä¸ºæ­£æ´æ°ï¼è¯·éæ°è¾å¥...\n");
+            fflush(stdin);
+            i = 0;
+            ch = getchar();
+        }
+        else
+        {
+            i = i * 10 + (ch - '0');
+            ch = getchar();
+        }
+    }
+    return i;
+}
+
+void insert() // æå¥è¿ç¨
+{
+    if (!ready)
+    {
+        ready = p;
+        pfend = p;
+    }
+    else
+    {
+        p->link = pfend->link;
+        pfend->link = p;
+        pfend = p;
+    }
+}
+
+void input() /*å»ºç«è¿ç¨æ§å¶åå½æ°*/
+{
+    int i, num;
+    printf("\nè¯·è¾å¥è¿ç¨çä¸ªæ°?\n");
+    num = geti();
+    for (i = 0; i < num; i++)
+    {
+        printf("\nè¿ç¨å·No.%d:\n", i + 1);
+        p = getpch(PCB);
+        printf("\nè¾å¥è¿ç¨å:");
+        scanf("%s", p->name);
+        printf("\nè¾å¥è¿ç¨è¿è¡æ¶é´:");
+        p->ntime = geti();
+        printf("\n");
+        p->rtime = 0;
+        p->state = 'w';
+        p->etime = TIME;
+        p->link = NOTHING;
+        insert();
+    }
+}
+
+void disp(PCB *pr) /*å»ºç«è¿ç¨ç°å®å½æ°ï¼ç¨äºæ¾ç¤ºå½åè¿ç¨*/
+{
+    printf("\nname\t state\t ntime\t rtime\t ltime\t \n");
+    printf("|%s\t", pr->name);
+    printf(" |%c\t", pr->state);
+    printf(" |%d\t", pr->ntime);
+    printf(" |%d\t", pr->rtime);
+    printf(" |%d\t", pr->etime);
+    printf("\n");
+}
+
+void check() /*å»ºç«è¿ç¨æ¥çå½æ°*/
+{
+    PCB *pr;
+    printf("\n ****å½åæ­£å¨è¿è¡çè¿ç¨æ¯:%s", ready->name); /*æ¾ç¤ºå½åè¿è¡çè¿ç¨*/
+    disp(ready);
+    pr = ready->link;
+    printf("\n****å½åå°±ç»ªéåç¶æä¸º:\n"); /*æ¾ç¤ºå°±ç»ªéåç¶æ*/
+    while (pr != NOTHING)
+    {
+        disp(pr);
+        pr = pr->link;
+    }
+}
+
+void rr() // è°æ´è¿ç¨éå
+{
+    if (!ready->link)
+        return;
+    p = ready;
+    ready = ready->link;
+    pfend = pfend->link = p;
+    pfend->link = NULL;
+    // å¾è¡¥å¨
+}
+
+void addnew() // æ·»å æ°çè¿ç¨
+{
+    ready->state = 'w';
+    input();
+}
+
+void destroy() /*å»ºç«è¿ç¨æ¤éå½æ°(è¿ç¨è¿è¡ç»æï¼æ¤éè¿ç¨)*/
+{
+    printf("\nè¿ç¨[%s]å·²å®æ.\n", ready->name);
+    p = ready;
+    ready = ready->link;
+    free(p);
+}
+
+void running() /*å»ºç«è¿ç¨å°±ç»ªå½æ°(è¿ç¨è¿è¡æ¶é´å°ï¼ç½®å°±ç»ªç¶æ)*/
+{
+    (ready->rtime)++;
+    (ready->etime)--;
+    check();
+    if (ready->rtime == ready->ntime)
+    {
+        destroy();
+        return;
+    }
+    else if (ready->etime == 0)
+    {
+        ready->state = 'w';
+        ready->etime = TIME;
+        rr();
+    }
+    // printf("1223");
+}
+
+void main()
+{
+    char ch;
+    input();
+    while (ready != NOTHING)
+    {
+        printf("\nThe execute name:%s\n", ready->name);
+        ready->state = 'R';
+        // check();
+        running();
+        printf("\næié®æ·»å æ°è¿ç¨....æå¶ä»ä»»æé®ç»§ç»­è¿è¡...");
+        fflush(stdin);
+        // printf("1111hi");
+        ch = getchar();
+        if (ch == 'i' || ch == 'I')
+            addnew();
+    }
+    printf("\n\n è¿ç¨å·²ç»å®æ\n");
+    getchar();
+}
